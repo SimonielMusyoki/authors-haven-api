05b5b2c2320498ee9c07b171a0e17e7e3afdf859
SimonielMusyoki <musyoki.mtk3@gmail.com>
2022-09-13 14:31:34 +0300

    chore: Configure Django Logginng

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from pydoc import resolve
 import environ
 
 env = environ.Env()
@@ -138,3 +137,24 @@ MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_URLS_REGEX = r"^/api/.*$"
+
+
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
