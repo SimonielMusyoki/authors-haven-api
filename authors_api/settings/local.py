6f0313df7af2e829042cd9c7b5a9b6735d9b04ba
SimonielMusyoki <musyoki.mtk3@gmail.com>
2022-09-16 15:02:31 +0300

    Feature: Add Celery, redis and Flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -9,3 +9,10 @@ SECRET_KEY = env(
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
